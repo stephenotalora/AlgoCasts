0074a3acfa50f1811cfb1c7528728e770575d747
Jonathan S. Otalora <j.otalora@me.com>
2018-10-28 21:00:48 -0700

    adds maxchar solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,28 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// O(n)
+function maxChar(str) {
+  const set = {};
 
-module.exports = maxChar;
+  for (const char of str) {
+    if (set[char] >= 0) set[char] += 1;
+    else set[char] = 1;
+  }
+
+  let result = '', reps = 0;
+  for (const char in set) {
+    if (set[char] > reps) {
+      result = char;
+      reps = set[char]
+    }
+  }
+
+  return result;
+}
+
+// tests from examples
+// console.log(maxChar("abcccccccd"));
+// console.log(maxChar("apple 1231111"));
+
+module.exports = { maxChar };
--- exercises/maxchar/test.js
+++ exercises/maxchar/test.js
@@ -1,4 +1,4 @@
-const maxChar = require('./index');
+const { maxChar } = require('./index');
 
 test('maxChar function exists', () => {
   expect(typeof maxChar).toEqual('function');
