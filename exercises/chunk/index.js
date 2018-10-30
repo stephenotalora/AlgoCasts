d976ed28fb115317c4c906f06706495658c6f084
Jonathan S. Otalora <j.otalora@me.com>
2018-10-29 22:56:55 -0700

    adds alternative solutions to chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,15 +8,48 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
+// O(n) mutable solution
+// function chunk(array, size) {
+//   const result = [];
+
+//   while(array.length) {
+//     result.push(array.splice(0, size));
+//   }
+
+//   return result;
+// }
+
+// O(n) immutable solution
 function chunk(array, size) {
   const result = [];
+  let index = 0;
 
-  while(array.length) {
-    result.push(array.splice(0, size));
+  while (index < array.length) {
+    result.push(array.slice(index, index + size));
+    index += size;
   }
 
   return result;
 }
+
+// O(n) solution
+// function chunk(array, size) {
+//   const result = [];
+//   const rem = array.length % size;
+
+//   let chunk = [];
+//   for (let i = 0; i < array.length; ++i) {
+//     chunk.push(array[i]);
+    
+//     if (chunk.length === size || 
+//       (i + 1 >= array.length - rem && chunk.length === rem)) {
+//       result.push(chunk);
+//       chunk = [];
+//     }
+//   }
+
+//   return result;
+// }
 
 // tests from example
 // console.log(chunk([1, 2, 3, 4], 2));
