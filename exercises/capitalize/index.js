8631bbcb595f0ed05e9ad17186e3c6f29520a3d1
Jonathan S. Otalora <j.otalora@me.com>
2018-10-31 20:19:28 -0700

    capitalization exercise

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,26 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str.charAt(0).toUpperCase();
+  
+  for (let i = 1; i < str.length; ++i) {
+    result += str.charAt(i - 1) === ' ' ? 
+      str.charAt(i).toUpperCase() : 
+      str.charAt(i).toLowerCase()
+  }
 
-module.exports = capitalize;
+  return result;
+}
+
+// function capitalize(str) {
+//   return str.split(' ').map((word) => (
+//     `${word.charAt(0).toUpperCase()}${word.slice(1).toLowerCase()}`
+//   )).join(' ');
+// }
+
+// console.log(capitalize('a short sentence'));
+// console.log(capitalize('a lazy fox'));
+// console.log(capitalize('look, it is working!'));
+
+module.exports = { capitalize };
--- exercises/capitalize/test.js
+++ exercises/capitalize/test.js
@@ -1,4 +1,4 @@
-const capitalize = require('./index');
+const { capitalize } = require('./index');
 
 test('Capitalize is a function', () => {
   expect(typeof capitalize).toEqual('function');
